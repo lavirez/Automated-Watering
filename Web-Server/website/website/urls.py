4e6e476d8adfb7c008adda21672db7c9b1391ca8
alireza-a-razavi <alirezaalavirazavi@yahoo.com>
2021-03-21 11:02:22 +0330

    hotfix1

--- Web-Server/website/website/urls.py
+++ Web-Server/website/website/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', name="main")),
+    path('', include('main.urls', namespace="main")),
 ]
 
 if settings.DEBUG: 
